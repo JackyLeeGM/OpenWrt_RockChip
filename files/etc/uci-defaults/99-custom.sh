a1e1f57251f18d854aba4fa6afbcb76591adc61c
JackyLee <39452500+JackyLeeGM@users.noreply.github.com>
2025-09-02 15:16:21 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -95,6 +95,8 @@ elif [ "$count" -gt 1 ]; then
 
     # LAN口设置静态IP
     uci set network.lan.proto='static'
+    # 开启强制DHCP服务
+    uci set dhcp.lan.force='1'
     # 多网口设备 支持修改为别的管理后台地址 在Github Action 的UI上自行输入即可 
     uci set network.lan.netmask='255.255.255.0'
     # 设置路由器管理后台地址
@@ -106,8 +108,6 @@ elif [ "$count" -gt 1 ]; then
         echo "custom router ip is $CUSTOM_IP" >> $LOGFILE
     else
         uci set network.lan.ipaddr='192.168.6.1'
-        # 开启强制DHCP服务
-        uci set dhcp.lan.force='1'
         echo "default router ip is 192.168.6.1" >> $LOGFILE
     fi
 
