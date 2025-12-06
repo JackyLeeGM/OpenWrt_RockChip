bdfa55692d5e7407515a08ed331c56f9950fc525
JackyLee <39452500+JackyLeeGM@users.noreply.github.com>
2025-12-06 17:21:35 +0800

    Update build24.sh

--- rockchip/build24.sh
+++ rockchip/build24.sh
@@ -72,6 +72,36 @@ fi
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # 静态文件服务器dufs(推荐)
 PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn"
+# 以下悟空默认插件后 再次添加的插件
+PACKAGES="$PACKAGES luci-i18n-cpufreq-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-openlist-zh-cn"
+# 服务——FileBrowser 用户名admin 密码admin
+PACKAGES="$PACKAGES luci-i18n-filebrowser-go-zh-cn"
+# 增加几个必备组件 方便用户安装iStore
+PACKAGES="$PACKAGES fdisk"
+PACKAGES="$PACKAGES script-utils"
+PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
+# 添加ax网卡和mt792x网卡驱动包测试是否可行
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-iwlwifi"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES iwlwifi-firmware-ax200"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES iwlwifi-firmware-ax201"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES iwlwifi-firmware-ax210"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES mt7921bt-firmware"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-mt7921u"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-mt7921-common"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-mt7921-firmware"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES mt7922bt-firmware"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-mt7922-firmware"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-mt76-core"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-mt76-connac"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-mt792x-common"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-mt76-usb"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-mt792x-usb"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-hwmon-core"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-mmc"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-mt76-sdio"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-mt7921s"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-mt7921e"
 # ======== shell/custom-packages.sh =======
 # 合并imm仓库以外的第三方插件
 PACKAGES="$PACKAGES $CUSTOM_PACKAGES"
